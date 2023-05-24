96c22181b65261d9943453ea932283fe7e873480
Gregory Mullen <_@gr.ht>
2023-05-24 02:02:11 -0700

    Add new state saving system

---
+++ src/state.zig
@@ -0,0 +1,20 @@
+const HSH = @import("hsh.zig").HSH;
+
+const State = @This();
+
+name: []const u8,
+ctx: *anyopaque,
+api: *const API,
+
+pub const API = struct {
+    /// each line returned must be allocated by `h.alloc` because it will be
+    /// freed by the same once written.
+    /// Save will only insert whitespace between sections, and will not
+    /// otherwise attemte to modify the data in any way. API is required to
+    /// provide it's own white space where required.
+    save: *const fn (h: *HSH, _: *anyopaque) ?[][]const u8,
+};
+
+pub fn save(self: *State, h: *HSH) ?[][]const u8 {
+    return self.api.save(h, self.ctx);
+}
