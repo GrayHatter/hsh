93990099455b09ac444ecb70952da66c984b7e07
Gregory Mullen <_@gr.ht>
2023-08-24 10:34:45 -0700

    add not implemented notice to pipeline bi

--- src/builtins/pipeline.zig
+++ src/builtins/pipeline.zig
@@ -3,6 +3,7 @@ const hsh = @import("../hsh.zig");
 const HSH = hsh.HSH;
 const ParsedIterator = @import("../parse.zig").ParsedIterator;
 const bi = @import("../builtins.zig");
+const print = bi.print;
 const Err = bi.Err;
 
 pub fn init() void {}
@@ -14,5 +15,6 @@ fn file() !std.fs.File {}
 pub fn pipeline(h: *HSH, titr: *ParsedIterator) Err!u8 {
     _ = h;
     _ = titr;
+    try print("pipeline is not yet implemented\n", .{});
     return 0;
 }
