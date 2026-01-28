ad21ab15bdbb10e92794e4ad4aa5f4d7007b0984
Gregory Mullen <_@gr.ht>
2026-01-27 20:17:23 -0800

    use StringHashMap for alias
    used arraylist previously, assuming HashMap wouldn't have the small list optimization

--- src/builtins/alias.zig
+++ src/builtins/alias.zig
@@ -1,6 +1,6 @@
 /// name and value are assumed to be owned by alias, and are expected to be
 // TODO this needs to become a map :/
-pub var aliases: ArrayList(Alias) = .{};
+var aliases: StringHashMap([]const u8) = .{};
 
 /// valid between calls to alias.
 pub const Alias = struct {
@@ -15,100 +15,86 @@ pub const Alias = struct {
 pub fn init() void {}
 
 pub fn raze(a: Allocator) void {
-    for (aliases.items) |ar| {
-        a.free(ar.name);
-        a.free(ar.value);
+    var itr = aliases.iterator();
+    while (itr.next()) |entry| {
+        a.free(entry.key_ptr.*);
+        a.free(entry.value_ptr.*);
     }
     aliases.deinit(a);
+    // Required for tests :/
+    aliases = .{};
 }
 
 pub fn save(_: *Hsh, w: *Writer) !void {
-    for (aliases.items) |al| {
-        try w.print("alias {f}\n", .{al});
+    var itr = aliases.iterator();
+    while (itr.next()) |entry| {
+        try w.print("alias {s}='{s}'\n", .{ entry.key_ptr.*, entry.value_ptr.* });
     }
 }
 
+fn validateName(str: []const u8) ![]const u8 {
+    // TODO validate input str
+    return str;
+}
+
 pub fn call(_: *Hsh, titr: *ParsedIterator, a: Allocator, _: Io) Err!u8 {
-    if (!std.mem.eql(u8, "alias", titr.first().resolved.str)) return Err.InvalidCommand;
+    std.debug.assert(eql(u8, "alias", titr.first().resolved.str));
+    const arg = titr.next() orelse return printAll();
 
-    var name: ?[]const u8 = null;
-    var value: ?[]const u8 = null;
-    const mode: ?[]const u8 = null;
-    while (titr.next()) |t| {
-        if (name) |_| {
-            value = t.resolved.str;
+    if (findScalar(u8, arg.resolved.str, '=')) |idx| {
+        const name = validateName(arg.resolved.str[0..idx]) catch unreachable;
+        const value = arg.resolved.str[idx + 1 ..];
+        if (add(name, value, a)) return 0 else |err| return err;
+    } else {
+        const name = validateName(arg.resolved.str) catch unreachable;
+        if (find(name)) |n| {
+            try print("{}\n", .{n});
         } else {
-            if (std.mem.indexOf(u8, t.resolved.str, "=")) |i| {
-                name = t.resolved.str[0..i];
-                if (t.resolved.str.len > i + 1) {
-                    value = t.resolved.str[i + 1 ..];
-                    break;
-                }
-            } else {
-                name = t.resolved.str;
-            }
-        }
-    }
-
-    if (mode) |_| unreachable; // not implemented;
-
-    if (name) |n| {
-        if (value) |v| {
-            if (try replace(a, n, v)) return 0;
-            if (add(a, n, v)) return 0 else |err| return err;
-        }
-        if (find(n)) |nn| {
-            try print("{}\n", .{nn});
-        } else {
-            try print("no known alias for {s}\n", .{n});
+            try print("no known alias for {s}\n", .{name});
         }
         return 0;
     }
+}
 
-    for (aliases.items) |al| {
-        try print("{}\n", .{al});
+fn printAll() Err!u8 {
+    var itr = aliases.iterator();
+    while (itr.next()) |entry| {
+        try print("{s}={s}\n", .{ entry.key_ptr.*, entry.value_ptr.* });
     }
     return 0;
 }
 
 /// alias retains ownership of all memory, and memory lifetime is undefined
-pub fn find(src: []const u8) ?*Alias {
-    for (aliases.items) |*a| {
-        if (std.mem.eql(u8, src, a.name)) {
-            return a;
-        }
+pub fn find(name: []const u8) ?Alias {
+    if (aliases.getEntry(name)) |entry| {
+        return .{ .name = entry.key_ptr.*, .value = entry.value_ptr.* };
     }
     return null;
 }
 
 // TODO might leak
-fn add(a: Allocator, src: []const u8, dst: []const u8) Err!void {
-    log.debug("ALIAS adding {s} = '{s}'\n", .{ src, dst });
-    if (dst.len == 0) return del(src, a);
-    try aliases.append(a, .{
-        .name = try a.dupe(u8, src),
-        .value = try a.dupe(u8, dst),
-    });
-}
-
-/// Returns true IFF existing value is replaced
-fn replace(a: std.mem.Allocator, key: []const u8, val: []const u8) !bool {
-    if (find(key)) |*found| {
-        a.free(found.*.value);
-        found.*.value = try a.dupe(u8, val);
-        return true;
+fn add(name: []const u8, val: []const u8, a: Allocator) Err!void {
+    log.debug("ALIAS adding {s} = '{s}'\n", .{ name, val });
+    if (val.len == 0) return del(name, a);
+    const gop = aliases.getOrPut(a, name) catch unreachable;
+    if (gop.found_existing) {
+        a.free(gop.value_ptr.*);
+        gop.value_ptr.* = try a.dupe(u8, val);
+    } else {
+        gop.key_ptr.* = try a.dupe(u8, name);
+        gop.value_ptr.* = try a.dupe(u8, val);
     }
-    return false;
 }
 
-fn del(src: []const u8, a: Allocator) Err!void {
-    for (aliases.items, 0..) |al, i| {
-        if (std.mem.eql(u8, src, al.name)) {
-            const d = aliases.swapRemove(i);
-            a.free(d.name);
-            a.free(d.value);
-            return;
-        }
+pub fn testingAdd(name: []const u8, value: []const u8, a: Allocator) void {
+    add(name, value, a) catch unreachable;
+}
+
+fn del(name: []const u8, a: Allocator) Err!void {
+    if (aliases.getEntry(name)) |entry| {
+        a.free(entry.key_ptr.*);
+        a.free(entry.value_ptr.*);
+        std.debug.assert(aliases.remove(name));
     }
 }
 
@@ -117,7 +103,7 @@ test "alias" {
     init();
     defer raze(a);
 
-    try std.testing.expectEqual(aliases.items.len, 0);
+    try std.testing.expectEqual(aliases.count(), 0);
 }
 
 test save {
@@ -137,17 +123,19 @@ test save {
     const res = call(undefined, &pitr, a, io);
     try std.testing.expectEqual(res, 0);
 
-    try std.testing.expectEqual(aliases.items.len, 1);
-    const tst = try std.fmt.allocPrint(a, "alias {f}\n", .{aliases.items[0]});
+    try std.testing.expectEqual(1, aliases.count());
+    const tst = try std.fmt.allocPrint(a, "alias {f}\n", .{find("haxzor").?});
     defer a.free(tst);
     try std.testing.expectEqualStrings(str, tst[0 .. tst.len - 1]); // strip newline
 }
 
 const std = @import("std");
 const Allocator = std.mem.Allocator;
-const ArrayList = std.ArrayList;
+const StringHashMap = std.hash_map.StringHashMapUnmanaged;
 const Io = std.Io;
 const Writer = Io.Writer;
+const eql = std.mem.eql;
+const findScalar = std.mem.findScalar;
 
 const Hsh = @import("../hsh.zig");
 const Token = @import("../token.zig");
